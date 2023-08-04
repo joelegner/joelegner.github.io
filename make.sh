74db98cb0a429472f42195e66bfe41743255e8ab
Joe Legner <joelegner@gmail.com>
2023-08-04 16:27:20 -0400

    make.sh script finds files instead of hard-coding

--- make.sh
+++ make.sh
@@ -1,5 +1,17 @@
-pandoc -s -o index.html index.md
-pandoc -s -o subpage1.html subpage1.md
-pandoc -s -o subpage2.html subpage2.md
-pandoc -s -o subpage3.html subpage3.md
+#!/bin/zsh
+
+# Step 1: Find all files matching the pattern '*.md'
+for sourcefile in *.md; do
+  # Check if there are any files matching the pattern
+  if [[ -e "$sourcefile" ]]; then
+    # Step 2: Convert each file to HTML using Pandoc
+    echo "$sourcefile -> ${sourcefile%.md}.html"
+    pandoc -s -o "${sourcefile%.md}.html" "$sourcefile"
+  else
+    echo "No files found matching the pattern '*.md'"
+    exit 1
+  fi
+done
+
+# Step 3: Open index.html
 open index.html
